ffe78cbd7f71ccc3736696d932cc67b007328318
Anderson Palacio <Anderson.Palacio@endava.com>
2019-12-26 16:11:59 -0500

    correccion defauldChecked

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ class App extends Component {
     terms : false
   }
 
-  handleClick = (event) => (
+  handleClick(event){ 
     this.setState({
       terms : event.target.checked
     })
-  )
+}
 
 
 
   render() {
     return (
       <div className="wrapper">
-        <label><input type="checkbox" checked={this.state.terms} onClick={this.handleClick}/> Mostrar información importante</label>
+        <label><input type="checkbox"  defaultChecked={this.state.terms} onClick={this.handleClick.bind(this)}  /> Mostrar información importante</label>
         {this.state.terms ? <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> : null}
       </div>
     );
